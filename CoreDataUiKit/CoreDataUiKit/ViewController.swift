aae2717bc04342faaeebf5ef0a84e0b49bf8b9e1
gabrieldigon <gabrieldiasgonca@gmail.com>
2022-06-29 15:34:40 -0400

    Tela pronta-First Commit

---
+++ CoreDataUiKit/CoreDataUiKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataUiKit
+//
+//  Created by Gabriel Dias Goncalves on 28/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
